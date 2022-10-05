0f7230d282961611e1bb0e2a688023fdcd3b363f
bourisdim <bourisdim@outlook.com>
2022-10-05 18:56:15 +0300

    Creating jpa domain Entities

---
+++ src/main/java/com/bouris/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,40 @@
+package com.bouris.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipesNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipesNote() {
+        return recipesNote;
+    }
+
+    public void setRecipesNote(String recipesNote) {
+        this.recipesNote = recipesNote;
+    }
+}
